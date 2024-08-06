f360f12d31876c0908696995b6ab2e3d31159fc9
Gyanendra <116753180+Gyanendra145@users.noreply.github.com>
2024-08-06 10:23:23 +0000

    api

--- 02_controlFlow/one.js
+++ 02_controlFlow/one.js
@@ -4,49 +4,50 @@
     
 // }
 
-const isUserloggedIn = true
-const temperature = 41
+// const isUserloggedIn = true
+// const temperature = 41
 
-if (temperature === 41) {
-    console.log("less than 50");
-} else{
-    console.log("temperature is greater tha 50");
-}
+// if (temperature === 41) {
+//     console.log("less than 50");
+// } else{
+//     console.log("temperature is greater tha 50");
+// }
 
-console.log("execute");
-// <,>, <= , == , ===, !==
+// console.log("execute");
+// // <,>, <= , == , ===, !==
 
-const score = 200
+// const score = 200
 
-if(score>100){
-   var power = "fly"
-    console.log(`user power: ${power}`);
-}
-console.log(`user power: ${power}`);
+// if(score>100){
+//    var power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
 
-const balance = 1000
+// const balance = 1000
 
-if (balance<500) {
-    console.log("less than 500");
-} else if(balance < 750){
-    console.log("less than 750");
-} else if(balance < 1200
-){
+// if (balance<500) {
+//     console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750");
+// } else if(balance < 1200
+// ){
     
-    console.log("less than 1200");
-}
+//     console.log("less than 1200");
+// }
 
-const userLoggedIn = true
-const debitCard = true
+// const userLoggedIn = true
+// const debitCard = true
 
-const userLoggedInFromGoogle = false
-const loggedInfromEmail = true
+// const userLoggedInFromGoogle = false
+// const loggedInfromEmail = true
 
 
-if (userLoggedIn && debitCard && 2==2) {
-    console.log("Allow to buy course");
-}
+// if (userLoggedIn && debitCard && 2==2) {
+//     console.log("Allow to buy course");
+// }
 
-if (userLoggedInFromGoogle || loggedInfromEmail) {
-    console.log("user logged In");
-}
+// if (userLoggedInFromGoogle || loggedInfromEmail) {
+//     console.log("user logged In");
+// }
+
