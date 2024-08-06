6c5941a84c8871c3b4c2a53f6ad1817fd0b149ed
Gyanendra <116753180+Gyanendra145@users.noreply.github.com>
2024-08-06 13:35:05 +0000

    promises

--- .devcontainer/promises/promises.js
+++ .devcontainer/promises/promises.js
@@ -50,20 +50,62 @@ const username= promiseFour.then((user)=>{
     console.log(error);   
 }).finally(()=> console.log("The promise is either resolved or rejected"))
 
-const promiseFive= new Promise(function(resolve, reject){
+// const promiseFive= new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         let error = true
+//         if(!error){
+//             resolve({username:"gyansingh", passwoed: "123456"})
+//         } else{
+//             reject('ERROR: JS went wrong')
+//         }
+//     }, 1000)
+// })
+
+// async function consumePromiseFive(){
+//     try{
+//     const response = await promiseFive
+//     console.log(response);
+// } catch(error){
+//     console.log(error);
+// }
+// }
+// consumePromiseFive()
+
+// async function getAllUsers() {
+//     const response = await fetch('https://github.com/Gyanendra145/JS_advance')
+//     const data = response.json()
+//     console.log(data);  
+// }
+// getAllUsers()
+
+const promiseFive = new Promise(function(resolve, reject){
     setTimeout(function(){
-        let error = true
+        let error = false; // Set this to false to test the resolve case
         if(!error){
-            resolve({username:"gyansingh", passwoed: "123456"})
+            resolve({username:"gyansingh", password: "123456"}) // Corrected property name
         } else{
             reject('ERROR: JS went wrong')
         }
     }, 1000)
-})
+});
 
 async function consumePromiseFive(){
-    const response = await promiseFive
-    console.log(response);
+    try{
+        const response = await promiseFive;
+        console.log(response);
+    } catch(error){
+        console.log(error);
+    }
 }
+consumePromiseFive();
 
-consumePromiseFive()
+async function getAllUsers() {
+    try {
+        const response = await fetch('https://your-api-url.com/endpoint'); // Replace with actual API URL
+        const data = await response.json(); // Added await here
+        console.log(data);  
+    } catch (error) {
+        console.log('Error fetching users:', error);
+    }
+}
+getAllUsers();
